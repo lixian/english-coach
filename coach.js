d63f25e4264c29a28d6e5c5c1b88ae185e0f6857
lixian <pkuloki@163.com>
2013-08-11 04:08:49 +0800

    refactoring set

--- coach.js
+++ coach.js
@@ -1,18 +1,23 @@
 // global funcitons
+<<<<<<< HEAD
+var Set = function() { this._map = {}; }
+Set.prototype.add = function(k) { this._map[k] = true; }
+Set.prototype.remove = function(k) { delete this._map[k]; }
+Set.prototype.contains = function(k) { return this._map[k] != undefined }
+Set.prototype.keys = function() { return Object.keys(this._map); }
+Set.prototype.each = function(callback) { this.keys().forEach(callback); } 
+Set.prototype.size = function() { return this.keys().length; }
+Set.prototype.addAll = function(arr) { var self = this; arr.forEach(function(k) { self.add(k); }); }
+=======
 var Set = function() {}
 Set.prototype._add = function(o) { this[o] = true; }
 Set.prototype._remove = function(o) { delete this[o]; }
 Set.prototype._contains = function(o) { return this[o] != undefined }
 Set.prototype._keys = function() { return Object.keys(this); }
 Set.prototype._addArray = function(o) { for (var i in o) { this._add(o[i]); } } 
+>>>>>>> 7fa7748ec3ae61d0df773650163b9c96eadbb0d4
 
 function log(obj) { console.log(obj); } 
-function addCount(map, word) {
-    if (map[word] == undefined) {
-        map[word] = 0;
-    }
-    map[word]++;
-}
 
 function replaceWord(word, dest) {
     var reg = new RegExp("\\b" + word + "\\b");
@@ -30,21 +35,38 @@ function highlight(word) {
 
 // known words
 var knownSet = new Set();
-
 // add cet-4
-knownSet._addArray(cet4_keys);
-log("after add cet-4, known count = " + knownSet._keys().length);
+knownSet.addAll(cet4_keys);
+log("after add cet-4, known count = " + knownSet.size());
 // add junior high
-knownSet._addArray(junior_high_keys);
-log("after add junior high, known count = " + knownSet._keys().length);
+knownSet.addAll(junior_high_keys);
+log("after add junior high, known count = " + knownSet.size());
 // add personal custom words
 if (typeof(custom_keys) !== "undefined") {
-    knownSet._addArray(custom_keys);
-    log("personal custom keys: " + custom_keys);
-    log("after add custom words, known count = " + knownSet._keys().length);
+    knownSet.addAll(custom_keys);
+    log("after add custom words, known count = " + knownSet.size());
 }
 
 // article
+<<<<<<< HEAD
+var articleSet = new Set();
+var words = $("p").text().split(/[^a-zA-Z]/);
+articleSet.addAll(words);
+articleSet.remove("")
+log("article words count = " + articleSet.size());
+
+// unknown words
+var unknownSet = new Set();
+articleSet.each(function(word) {
+    if (!knownSet.contains(word.toLowerCase())) {
+        unknownSet.add(word);
+    }
+});
+log("unknown words count = " + unknownSet.size());
+
+// highlight unknown words
+unknownSet.each(function(word) {
+=======
 var s = $("p").text();
 var words = s.split(/[^a-zA-Z]/);
 var articleSet = new Set();
@@ -65,6 +87,7 @@ log("unknown words count = " + unknownKeys.length);
 
 // highlight unknown words
 unknownKeys.forEach(function(word) {
+>>>>>>> 7fa7748ec3ae61d0df773650163b9c96eadbb0d4
     highlight(word);
 });
 
