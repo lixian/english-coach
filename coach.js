bf71fa6b894e96e09bcef5b31c4c5361b76cdf60
lixian <pkuloki@163.com>
2013-08-11 04:13:25 +0800

    refactoring set

--- coach.js
+++ coach.js
@@ -1,5 +1,4 @@
 // global funcitons
-<<<<<<< HEAD
 var Set = function() { this._map = {}; }
 Set.prototype.add = function(k) { this._map[k] = true; }
 Set.prototype.remove = function(k) { delete this._map[k]; }
@@ -8,14 +7,6 @@ Set.prototype.keys = function() { return Object.keys(this._map); }
 Set.prototype.each = function(callback) { this.keys().forEach(callback); } 
 Set.prototype.size = function() { return this.keys().length; }
 Set.prototype.addAll = function(arr) { var self = this; arr.forEach(function(k) { self.add(k); }); }
-=======
-var Set = function() {}
-Set.prototype._add = function(o) { this[o] = true; }
-Set.prototype._remove = function(o) { delete this[o]; }
-Set.prototype._contains = function(o) { return this[o] != undefined }
-Set.prototype._keys = function() { return Object.keys(this); }
-Set.prototype._addArray = function(o) { for (var i in o) { this._add(o[i]); } } 
->>>>>>> 7fa7748ec3ae61d0df773650163b9c96eadbb0d4
 
 function log(obj) { console.log(obj); } 
 
@@ -48,7 +39,6 @@ if (typeof(custom_keys) !== "undefined") {
 }
 
 // article
-<<<<<<< HEAD
 var articleSet = new Set();
 var words = $("p").text().split(/[^a-zA-Z]/);
 articleSet.addAll(words);
@@ -66,28 +56,6 @@ log("unknown words count = " + unknownSet.size());
 
 // highlight unknown words
 unknownSet.each(function(word) {
-=======
-var s = $("p").text();
-var words = s.split(/[^a-zA-Z]/);
-var articleSet = new Set();
-articleSet._addArray(words);
-articleSet._remove("")
-articleKeys = articleSet._keys();
-log("article words count = " + articleKeys.length);
-
-// unknown words
-var unknownSet = new Set();
-articleKeys.forEach(function(word) {
-    if (!knownSet._contains(word.toLowerCase())) {
-        unknownSet._add(word);
-    }
-});
-var unknownKeys = unknownSet._keys();
-log("unknown words count = " + unknownKeys.length);
-
-// highlight unknown words
-unknownKeys.forEach(function(word) {
->>>>>>> 7fa7748ec3ae61d0df773650163b9c96eadbb0d4
     highlight(word);
 });
 
